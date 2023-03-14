fe863565c41ee2e9effa0d935422dcee8a1d19fa
michinoins <m1i1k2i1ya@gmail.com>
2023-03-14 15:07:29 -0700

    add: mock data for get

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,20 @@ import express from "express";
 import apiRouter from "./routes/api";
 
 const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
 
 // Routes
 app.use("/", apiRouter);
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(8080, () => {
+  console.log("Server listening on port 8080");
 });
 
 export default app;
